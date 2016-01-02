643ca0bed1db6c596e18e93972b68b7b15fd2da8
JeffGLFC <jmcaulay@glfc.org>
2016-01-02 01:29:12 +0100

    Added stream import and ajax services

---
+++ dist/ajax/getStreamPath.php
@@ -0,0 +1,21 @@
+<?php
+	if(isset($_GET['id']) && $_GET['id'] != '')
+	{
+		$key = $_GET['id'];
+
+		require "../predis/autoload.php";
+
+		$my_server = array(
+		    'host'     => '127.0.0.1',
+		    'port'     => 6379,
+		    'database' => 15
+		);
+
+		$client = new Predis\Client($my_server);
+
+		$path = $client->get($key);
+
+		print $path;
+
+	}
+?>
